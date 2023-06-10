ea439e8c0ea6909ac3d7612cb52b70acced72380
swapnilaanam <swapnilaanam211@gmail.com>
2023-06-11 02:12:42 +0600

    added ManageUser.jsx for admin dashboard

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import ActiveLink from '../pages/Shared/ActiveLink/ActiveLink';
 import { Outlet } from 'react-router-dom';
 import useAdmin from '../hooks/useAdmin';
+import NavBar from '../pages/Shared/Navbar/NavBar';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const Dashboard = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    // const [isAdmin] = useAdmin();
-    const isAdmin = true;
+    const [isAdmin] = useAdmin();
     const isInstructor = false;
 
     const toggleDrawer = () => {
@@ -15,30 +16,34 @@ const Dashboard = () => {
     }
 
     return (
-        <div className="drawer lg:drawer-open">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-                <Outlet></Outlet>
+        <>
+            <NavBar></NavBar>
+            <div className="drawer lg:drawer-open">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    <Outlet></Outlet>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 h-full bg-sky-500 text-white">
+                        {
+                            isAdmin ? <>
+                                <li><ActiveLink to="/dashboard/manageclasses">Manage Classes</ActiveLink></li>
+                                <li><ActiveLink to="/dashboard/manageusers">Manage Users</ActiveLink></li>
+                            </> : isInstructor ? <>
+                                <li><ActiveLink to="/dashboard/addclass">Add A Class</ActiveLink></li>
+                                <li><ActiveLink to="dashboard/myclasses">My Classes</ActiveLink></li>
+                            </> : <>
+                                <li><ActiveLink to="/dashboard/myselectedclasses">My Selected Classes</ActiveLink></li>
+                                <li><ActiveLink to="/dashboard/myenrolledclasses">My Enrolled Classes</ActiveLink></li>
+                            </>
+                        }
+                    </ul>
+                </div>
             </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-full bg-green-500 text-white">
-                    {
-                        isAdmin ? <>
-                            <li><ActiveLink to="/dashboard/manageclasses">Manage Classes</ActiveLink></li>
-                            <li><ActiveLink to="/dashboard/manageusers">Manage Users</ActiveLink></li>
-                        </> : isInstructor ? <>
-                            <li><ActiveLink to="/dashboard/addclass">Add A Class</ActiveLink></li>
-                            <li><ActiveLink to="dashboard/myclasses">My Classes</ActiveLink></li>
-                        </> : <>
-                            <li><ActiveLink to="/dashboard/myselectedclasses">My Selected Classes</ActiveLink></li>
-                            <li><ActiveLink to="/dashboard/myenrolledclasses">My Enrolled Classes</ActiveLink></li>
-                        </>
-                    }
-                </ul>
-            </div>
-        </div>
+            <Footer></Footer>
+        </>
     );
 };
 
--- src/pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,7 +1,115 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
 const ManageUsers = () => {
+    const [axiosSecure] = useAxiosSecure();
+
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users');
+            return res.data;
+        }
+    });
+
+    const handleMakeInstructor = user => {
+        axiosSecure.patch(`/users/${user._id}`, {
+            role: 'instructor'
+        })
+            .then(res => {
+                console.log(res.data);
+                if (res.data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is Instructor Now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+            .catch(error => console.log(error));
+    }
+
+    const handleMakeAdmin = user => {
+        axiosSecure.patch(`/users/${user._id}`, {
+            role: 'admin'
+        })
+            .then(res => {
+                console.log(res.data);
+                if (res.data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is Admin Now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+            .catch(error => console.log(error));
+    }
+
     return (
-        <div>
-            <h2 className="text-black">Manage Users</h2>
+        <div className="w-full px-10 h-full">
+            <h2 className="text-black text-3xl font-medium text-center my-12">Manage Users</h2>
+            <div className="overflow-x-auto">
+                <table className="table text-center border-2 table-zebra">
+                    <thead className="bg-stone-400 text-black text-base">
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr
+                                key={user._id} className="border-2"
+                            >
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={user.profile} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {user.name}
+                                </td>
+                                <td>
+                                    {user.email}
+                                </td>
+                                <td>
+                                    {user.role}
+                                </td>
+                                <th>
+                                    <div className="flex gap-5">
+                                        <button onClick={() => handleMakeInstructor(user)} disabled={user.role === 'instructor'} className="btn btn-warning btn-sm">
+                                            Make Instructor
+                                        </button>
+                                        <button onClick={() => handleMakeAdmin(user)} disabled={user.role === 'admin'} className="btn btn-success btn-sm">
+                                            Make Admin
+                                        </button>
+                                    </div>
+                                </th>
+                            </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
