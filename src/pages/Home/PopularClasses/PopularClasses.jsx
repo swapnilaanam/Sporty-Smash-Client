c7319d7b579e66880112f499d8585285a84bfc45
swapnilaanam <swapnilaanam211@gmail.com>
2023-06-14 00:21:44 +0600

    added Framer-motion animation on my website

--- src/pages/Home/PopularClasses/PopularClasses.jsx
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { motion } from "framer-motion";
 
 const PopularClasses = () => {
 
@@ -13,38 +14,45 @@ const PopularClasses = () => {
     });
 
     return (
-        <div className="py-24">
+        <div className="py-24 px-4">
+
             <h2 className="text-center font-bold text-3xl uppercase mb-20">--- Popular Classes ---</h2>
             <div className="flex flex-col lg:flex-row justify-center items-center flex-wrap gap-14">
                 {
-                    popularClasses.map(popularClass => <div
-                        key={popularClass._id}
-                        className="card card-compact w-96 bg-base-100 shadow-xl"
+                    popularClasses.map(popularClass => <motion.div
+                        initial={{ opacity: 0, translateX: -150 }}
+                        whileInView={{ opacity: 1, translateX: 0 }}
+                        transition={{ duration: 1.8 }}
                     >
-                        <figure>
-                            <img src={popularClass.classImage} alt="Popular Class" className="w-full h-[288px] object-cover"/>
-                        </figure>
-                        <div className="card-body my-4 space-y-3">
-                            <h2 className="card-title">{popularClass.className}</h2>
-                            <p className="font-medium text-base">Instructor Name: {popularClass.instructorName}</p>
-                            <p className="font-medium text-base">
-                                Total Enrolled Students:
-                                <span className="badge badge-warning badge-lg ms-2 px-5 py-4">
-                                    {popularClass.totalEnrolledStudents}
-                                </span>
-                            </p>
-                            <p className="font-medium text-base">
-                                Available Seats:
-                                <span className="badge badge-neutral badge-lg ms-2 px-5 py-4 text-white">
-                                    {popularClass.availableSeats}
-                                </span>
-                            </p>
-                            <p className="font-medium text-base">
-                                Price:
-                                <span className="badge badge-success badge-lg ms-2 px-5 py-4">${popularClass.price}</span>
-                            </p>
+                        <div
+                            key={popularClass._id}
+                            className="card card-compact w-96 bg-base-100 shadow-xl"
+                        >
+                            <figure>
+                                <img src={popularClass.classImage} alt="Popular Class" className="w-full h-[288px] object-cover" />
+                            </figure>
+                            <div className="card-body my-4 space-y-3">
+                                <h2 className="card-title">{popularClass.className}</h2>
+                                <p className="font-medium text-base">Instructor Name: {popularClass.instructorName}</p>
+                                <p className="font-medium text-base">
+                                    Total Enrolled Students:
+                                    <span className="badge badge-warning badge-lg ms-2 px-5 py-4">
+                                        {popularClass.totalEnrolledStudents}
+                                    </span>
+                                </p>
+                                <p className="font-medium text-base">
+                                    Available Seats:
+                                    <span className="badge badge-neutral badge-lg ms-2 px-5 py-4 text-white">
+                                        {popularClass.availableSeats}
+                                    </span>
+                                </p>
+                                <p className="font-medium text-base">
+                                    Price:
+                                    <span className="badge badge-success badge-lg ms-2 px-5 py-4">${popularClass.price}</span>
+                                </p>
+                            </div>
                         </div>
-                    </div>)
+                    </motion.div>)
                 }
             </div>
         </div>
